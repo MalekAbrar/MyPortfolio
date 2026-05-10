065c2ee440631064048cf77c3ee7f9653c94bff7
abrar <abrarmalek99099@gmail.com>
2026-05-10 13:15:55 +0530

    Updated README and branding

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -3,9 +3,9 @@ export const contactsData = {
     phone: '+91 9426403370',
     address: 'Gopi Pura Momnawad Surat 395002 ',
     github: 'https://github.com/MalekAbrar',
-    facebook: 'https://www.facebook.com/',
+    facebook: 'https://www.facebook.com/abusaid.riyaz/',
     linkedIn: 'https://www.linkedin.com/in/abrar-malek-024018308/',
-    twitter: 'https://twitter.com/',
-    // stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
+    twitter: 'https://twitter.com/said7388',
+    stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
     // devUsername: "said7388"
 }
